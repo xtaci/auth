ac8707eb7673b0d2b16708484ecb32a97c3b7051
xtaci <daniel820313@gmail.com>
2015-07-20 06:50:25 +0000

    challenge uuid

--- src/auth/service.go
+++ src/auth/service.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"golang.org/x/net/context"
+	"regexp"
 )
 
 import (
@@ -15,12 +17,30 @@ const (
 	_port = ":50006"
 )
 
+var (
+	ERROR_METHOD_NOT_SUPPORTED = errors.New("method not supoorted")
+)
+
+var (
+	uuid_regexp = regexp.MustCompile(`[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`)
+)
+
 type server struct {
 }
 
 func (s *server) init() {
 }
 
-func (s *server) Auth(context.Context, *Auth_Certificate) (*Auth_Result, error) {
+func (s *server) Auth(ctx context.Context, cert *Auth_Certificate) (*Auth_Result, error) {
+	switch cert.Type {
+	case Auth_UUID:
+		if uuid_regexp.Match(cert.Proof) {
+			return &Auth_Result{true, nil}, nil
+		}
+	case Auth_PLAIN:
+	case Auth_TOKEN:
+	case Auth_FACEBOOK:
+	default:
+	}
 	return nil, nil
 }
